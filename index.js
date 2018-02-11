2637234e6fe1d56aaae31b2f3f4ee3c6fb12ab41
Leonid <leomalinovsky@gmail.com>
2018-02-11 03:24:23 +0200

    fourth commit

--- index.js
+++ index.js
@@ -1,62 +1,102 @@
-console.log(" ---> KievFridayCinema Bot <--- ");
+console.log(" ---> KievFridayCinema <--- ");
 
 console.log(" Здравствуйте, господа! ");
 
-// var date = new Date();
-// var options = {
-// 	weekday: 'long',
-// 	year: 'numeric',
-// 	month: 'numeric',
-// 	day: 'numeric',
-// 	timezone: 'UTC'
-// };
-// var nowDate = date.toLocaleString("en", options);
-// console.log(' В этот день: ' + nowDate);
-
+const date = new Date();
+const day = date.toLocaleString('en', {day: '2-digit'});
+const month = date.toLocaleString('en', {month: '2-digit'});
+const year = date.toLocaleString('en', {year: 'numeric'});
+const weekday = date.toLocaleString('en', {weekday: 'long'});
+const today = day + month + year;
+console.log(' Сегодня ' + weekday);
 console.log(" Вашему вниманию предлагаются следующие фильмы: ");
 
 const needle = require('needle');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-
 const URL = 'https://multiplex.ua/cinema/kyiv/prospect';
 
 needle.get(URL, function(err, data){
     if (err) throw err;
     const mltplxBody = new JSDOM(data.body);
     const mltplx = mltplxBody.window.document;
+    const informarion_list = mltplx.querySelectorAll('.cinema_inside[data-date="' + today + '"] div.film');
+	const informarion = Array.prototype.slice.call(informarion_list);
 
-    var informarion_list = mltplx.querySelectorAll('.cinema_inside[data-date="08022018"] div.film');
-	var informarion = Array.prototype.slice.call(informarion_list);
-
+	console.log(' ==================== ');
 	informarion.forEach(function(item){
-		var film_list = item.querySelectorAll('div.info a');
-		var film = Array.prototype.slice.call(film_list);
-		var time_list = item.querySelectorAll('div.ns p.time');
-		var time = Array.prototype.slice.call(time_list);
-		console.log(time);
+		let film_list = item.querySelectorAll('div.info a');
+		let film = Array.prototype.slice.call(film_list);
+		let time_list = item.querySelectorAll('div.ns p.time');
+		let time = Array.prototype.slice.call(time_list);
 
-		film.forEach(function(item){
-			console.log(' ===================================== ');
-			console.log(' -> "'+ item.textContent+'"');
+		function listAfter23 (){
+			film.forEach(function(item){
+				let name = item.textContent;
+				let url_ = "https://multiplex.ua" + item.href;
 
-			var url_ = "https://multiplex.ua"+item.href;
-			console.log("    link: "+url_);
-
-			time.forEach(function(item){
-				var seanceTime = item.textContent;
-				var seanceTimeSplit = seanceTime.split(':')[0];
-				if (seanceTimeSplit === '23' || seanceTimeSplit === '00') {
-					console.log(' +  ' + seanceTime);
-				}else{
-					console.log('    ' + seanceTime);
-				}
+				time.forEach(function(item){
+					let seanceTime = item.textContent;
+					let seanceTimeSplit = seanceTime.split(':')[0];
+					if (seanceTimeSplit === '23' || seanceTimeSplit === '00') {
+						console.log(' => ' + name);
+						console.log(' => ' + url_);
+						console.log('    ' + seanceTime);
+						console.log(' ==================== ');
+					}
+				});
 			});
-		});
+		};
 
+		function fullList () {
+			film.forEach(function(item){
+				let name = item.textContent;
+				let url_ = "https://multiplex.ua" + item.href;
+				console.log(' => ' + name);
+				console.log(' => ' + url_);
+				time.forEach(function(item){
+					let seanceTime = item.textContent;
+					let seanceTimeSplit = seanceTime.split(':')[0];
+					if (seanceTimeSplit === '23' || seanceTimeSplit === '00') {
+						console.log(' +  ' + seanceTime);
+					}else{
+						console.log('    ' + seanceTime);
+					}
+				});
+				console.log(' ==================== ');
+			});
+		};
+
+		if (weekday == 'Friday') {
+			listAfter23();
+		} else {
+			fullList();
+		}
 	});
+
 	console.log('  ');
 	console.log(' Приятного просмотра! ');
 	console.log('  ');
-	console.log('  ');
 });
+
+	//  console.log('----------------------------------------');
+	// informarion.forEach(function(item){
+	// 	let film_list = item.querySelectorAll('div.ns');
+	// 	let film = Array.prototype.slice.call(film_list);
+	// 	film.forEach(function(item){
+	// 		let name = item.getAttribute('data-name');
+	// 		let url_ = "https://multiplex.ua"+item.getAttribute('data-moviehref');
+	// 		let time_list = item.querySelectorAll('p.time');
+	// 		let time = Array.prototype.slice.call(time_list);
+	// 		time.forEach(function(item){
+	// 			let seanceTime = item.textContent;
+	// 			let seanceTimeSplit = seanceTime.split(':')[0];
+	// 			if (seanceTimeSplit === '23' || seanceTimeSplit === '00') {
+	// 				console.log(' => ' + name);
+	// 				console.log(' => ' + url_);
+	// 				console.log(' => ' + seanceTime);
+	// 				console.log('----------------------------------------');
+	// 			}
+	// 		});
+	// 	});
+	// });
